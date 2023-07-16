eb2fd2db536bd3e27db102f6b3a9652ab3743c5a
yanisAitlamine <yanis.aitlamine@gmail.com>
2023-07-16 09:55:43 +0000

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Get a list of all .c files in the current directory
-c_files=$(find . -maxdepth 1 -type f -name "*.c")
+c_files=$(find ./source -maxdepth 1 -type f -name "*.c")
 echo $c_files
 # Check if the user provided the name of the resulting executable
 if [ $# -eq 0 ]; then
     echo "Please provide the name of the resulting executable."
-    echo "Usage: ./compile.sh <executable_name>"
+    echo "Usage: ./build.sh <executable_name>"
     exit 1
 fi
 
-executable_name=$1
+executable_name=$name
 
 # Compile each .c file with gcc
 
-gcc -o $1 $c_files 
+gcc -o ./bin/$name $c_files 
 
