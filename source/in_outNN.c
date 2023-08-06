70da941e74bbc37edf694181d6f94a114c627a30
Yanis <yanis.aitlamine@gmail.com>
2023-08-06 19:30:35 +0200

    change read to in_outNN2

---
+++ source/in_outNN.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "in_outNN.h"
+
+// read from a stream into a chain for a set number of characters
+int read(FILE *stream, char *chain, size_t len) {
+
+    char *returnChar = NULL;
+
+    if (fgets(chain, len, stream)==NULL){
+        return 1;
+    }
+    returnChar = strchr(chain, '\n');
+    if (returnChar!=NULL){
+            *returnChar='\0';
+    }
+    freeBuffer();
+    return 0;
+}
+
+// read characters in the buffer until the end of the fil or \n
+void freeBuffer() {
+    int a=0;
+    while (a!='\n' && a!=EOF) {
+        a=getchar();
+    }
+}
+
+
